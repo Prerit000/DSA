be39d58bfffe07c5cf48c10f369bc1fa17cf6cff
Prerit00 <preritp52@gmail.com>
2025-11-10 17:44:26 +0530

    add new files

--- hii.java
+++
@@ -1,4 +0,0 @@
-public class hii {
-    public static void main(String[] args) {
-        System.out.println("Hii");  }
-}
---
+++ squarepattern.java
@@ -0,0 +1,8 @@
+public class squarepattern {
+    public static void main(String[] args) 
+    {
+        for(int line=1;line<=4;line+=1){
+            System.out.println("****");
+        }
+    }
+}
